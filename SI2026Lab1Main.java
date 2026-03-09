2cf8988617b1c82639a4cd8d6d669d68a630b6ac
Luka Apostolov <luka.apostolov@students.finki.ukim.mk>
2026-03-09 13:34:16 +0100

    Implement borrow book

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -67,7 +67,7 @@ class Library {
             if (book.getTitle().equalsIgnoreCase(title)) {
                 if (!book.isBorrowed()) {
                     book.setBorrowed(true);
-                    System.out.println("Book borrowed.");
+                    System.out.println("Borrowed successfully");
                 } else {
                     System.out.println("Book is already borrowed.");
                 }
@@ -131,6 +131,6 @@ public class SI2026Lab1Main {
 
         System.out.println("Search Clean Code: " + library.searchBookByTitle("Clean Code"));
         System.out.println("Search Harry Potter: " + library.searchBookByTitle("Harry Potter"));
-
+        library.borrowBook("The Hobbit");
     }
 }
