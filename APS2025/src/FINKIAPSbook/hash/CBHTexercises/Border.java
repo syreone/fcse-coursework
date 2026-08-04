5439fc97f3b42eefe3d97b18c3a7c0741577b963
syreone <luka.apostolov@students.finki.ukim.mk>
2026-08-04 17:04:20 +0200

    adding exercises

--- APS2025/src/FINKIAPSbook/hash/CBHTexercises/Border.java
+++ APS2025/src/FINKIAPSbook/hash/CBHTexercises/Border.java
@@ -1,4 +1,40 @@
 package FINKIAPSbook.hash.CBHTexercises;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Border {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, String> passports = new HashMap<>();
+        HashMap<String, String> namesChanged = new HashMap<>();
+
+        int n = Integer.parseInt(br.readLine().trim());
+        for (int i=0; i<n; i++) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            String passportID = tokens[0];
+            String passportName = tokens[1];
+            passports.put(passportID, passportName);
+        }
+
+        int m = Integer.parseInt(br.readLine().trim());
+        for (int i = 0; i < m; i++){
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            String name = tokens[0];
+            String changedName = tokens[1];
+            namesChanged.put(name, changedName);
+        }
+
+        String query = br.readLine();
+        String name = passports.get(query);
+        if (namesChanged.containsKey(name)) {
+            System.out.println("Not Allowed");
+        } else {
+            System.out.println("Allowed");
+        }
+    }
 }
